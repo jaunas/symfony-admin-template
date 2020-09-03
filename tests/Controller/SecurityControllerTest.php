48c26756dde8535affa52a9b3b732d6d4ecbe57f
Tomasz Kowalewski <93.tomek@gmail.com>
2020-09-03 16:17:51 +0200

    Login/logout tests

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -15,11 +15,53 @@ class SecurityControllerTest extends AuthorizedWebTestCase
 
     public function testLogin(): void
     {
-        $this->markTestIncomplete();
+        $crawler = $this->client->request('GET', '/login');
+        $this->assertTrue($this->client->getResponse()->isOk());
+
+        $form = $crawler->filter('form[name=login]')->form([
+            'login' => [
+                'email' => static::USER_EMAIL,
+                'password' => 'user'
+            ],
+        ]);
+
+        $this->client->submit($form);
+        $this->assertTrue($this->client->getResponse()->isRedirect('/'));
+
+        $this->client->request('GET', '/');
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    public function testLoginFail(): void
+    {
+        $crawler = $this->client->request('GET', '/login');
+        $this->assertTrue($this->client->getResponse()->isOk());
+
+        $form = $crawler->filter('form[name=login]')->form([
+            'login' => [
+                'email' => static::USER_EMAIL,
+                'password' => 'pass'
+            ],
+        ]);
+
+        $this->client->submit($form);
+        $this->assertFalse($this->client->getResponse()->isRedirect('/'));
+
+        $this->client->request('GET', '/');
+        $this->assertTrue($this->client->getResponse()->isRedirect('/login'));
     }
 
     public function testLogout(): void
     {
-        $this->markTestIncomplete();
+        $user = $this->getUserByEmail(static::USER_EMAIL);
+        $this->client->loginUser($user);
+
+        $this->client->request('GET', '/');
+        $this->assertTrue($this->client->getResponse()->isOk());
+
+        $this->client->request('GET', '/logout');
+
+        $this->client->request('GET', '/');
+        $this->assertTrue($this->client->getResponse()->isRedirect('/login'));
     }
 }
